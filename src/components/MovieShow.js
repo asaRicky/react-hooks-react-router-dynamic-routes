ba92d677930baf42f182a1e3d5c2b5576a8143e7
asaRicky <derrickomondi208@gmail.com>
2023-04-27 09:29:33 +0300

    completed assignment

---
+++ src/components/MovieShow.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MovieShow() {
+    return (
+        <div>
+            <h3>Movies show component</h3>
+        </div>
+    );
+}
+
+export default MovieShow;
---
+++ src/components/MoviesList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { link } from "react-router-dom";
+
+function MoviesList({ movies }) {
+    const renderMovies = Object.keys(movies).map((movieID) => (
+        <li key={movieID}>
+            <link to={`/movies/${movieID}`}>{movies[movieID].title}</link>
+        </li>
+    ));
+
+    return <ul>{renderMovies}</ul>
+}
+
+export default MoviesList;
